6d3469e2ada79d85cd0f660469492674c9ff539c
Pierre Chavaroche <info@chavaroche.de>
2020-08-06 07:32:24 +0200

    First publish

---
+++ src/app/person.spec.ts
@@ -0,0 +1,48 @@
+import { Person } from './person';
+
+describe('Test for person', () => {
+    let person: Person;
+
+    //Arrange
+    beforeEach(() => {
+
+    });
+
+    describe('test for data', () => {
+        it('attributes', () => {
+            //Act && Assert
+
+        })
+
+    });
+
+    describe('test for calcBMI', () => {
+        it('should return a string: down', () => {
+            
+        });
+
+        it('should return a string: normal', () => {
+            
+        });
+
+        it('should return a string: overweight', () => {
+            
+        });
+
+        it('should return a string: overweight level 1', () => {
+            
+        });
+
+        it('should return a string: overweight level 2', () => {
+            
+        });
+
+        it('should return a string: overweight level 3', () => {
+            
+        });
+
+        it('should return a string: no found', () => {
+            
+        })
+    });
+});
