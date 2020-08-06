1ac43d69d7717dbfb40d94f48fd745ea20dc91bd
Pierre Chavaroche <info@chavaroche.de>
2020-08-06 07:36:38 +0200

    Person Row Component Test added

--- src/app/person-row/person-row.component.spec.ts
+++ src/app/person-row/person-row.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonRowComponent } from './person-row.component';
+import { Person } from '../person';
+import { By } from '@angular/platform-browser';
 
 describe('PersonRowComponent', () => {
   let component: PersonRowComponent;
@@ -8,7 +10,7 @@ describe('PersonRowComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PersonRowComponent ]
+      declarations: [ PersonRowComponent ], // declare the test component
     })
     .compileComponents();
   }));
@@ -16,10 +18,44 @@ describe('PersonRowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonRowComponent);
     component = fixture.componentInstance;
+
+    component.person = new Person(
+      'nicolas',
+      'molina',
+      23,
+      60,
+      1.70
+    );
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it("should the name be 'nicolas'", ()=>{
+    
+  })
+
+  it("should the age be '23'", ()=>{
+    
+  })
+
+  it("should the name be 'nicolas' in template", ()=>{
+
+  })
+
+  it("should the name be 'otro nombre' in template when I change", ()=>{
+
+  })
+
+  it("should the age be '23' in template", ()=>{
+
+  })
+
+  it("should the age be '24' in template when I change", ()=>{
+
+  })
+
+})
